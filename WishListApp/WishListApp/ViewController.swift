412080055921fe5e9273e249a8ac717f07da0430
Paolo Reyes <reyes.paolo@360logix.com>
2017-03-22 09:10:02 +0800

    Initial Commit of WishList App

---
+++ WishListApp/WishListApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WishListApp
+//
+//  Created by Paolo Reyes on 22/03/2017.
+//  Copyright © 2017 360 Logix Solutions Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
