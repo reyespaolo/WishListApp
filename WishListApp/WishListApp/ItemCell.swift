5f6821d0ecdc204d91e0e3f103e9031a0805b3ad
Paolo Reyes <reyes.paolo@360logix.com>
2017-03-22 10:50:37 +0800

    Set up Table View

---
+++ WishListApp/WishListApp/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  WishListApp
+//
+//  Created by Paolo Reyes on 22/03/2017.
+//  Copyright © 2017 360 Logix Solutions Inc. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+  
+}
